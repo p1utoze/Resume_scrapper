0d202a038a68fbcf69449d72e894bbfeb7d69fc7
p1utoze <aditya.n.awati@gmail.com>
2023-03-06 00:01:29 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -20,7 +20,7 @@ def resume_links(page_data, d):
         uri = i['href']
         if re.search(r'^/job/view-resume+', uri):
             resume = up.urljoin('https://www.jobspider.com', uri)
-            print('Extracting page: ', resume, '\n......')
+            print('---->     Extracting page: ', resume, '\n    ......')
             for key, value in get_resume(resume):
                 if not d.get(key, None):
                     d[key] = [value]
@@ -45,15 +45,21 @@ def main ():
     url = 'https://www.jobspider.com/job/resume-search-results.asp/words_engineer/searchtype_1'
     site = requests.get(url)
     check_pages = {}
-    resume_doc = up.urljoin('https://www.jobspider.com', '/job/view-resume-78327.html')
+    # resume_doc = up.urljoin('https://www.jobspider.com', '/job/view-resume-78327.html')
     soup = BeautifulSoup(site.text, "lxml")
     links = soup.find_all('font')[13]
     d = resume_links(links, {})
-    print(d.keys())
+
     for page in fetch_pages(links):
         if not check_pages.get(page, None):
+            print('\nPage: ', page)
+            soup = BeautifulSoup(requests.get(page).text, 'lxml')
+            features = soup.find_all('font')[13]
+            d = resume_links(features, d)
+            print('Resumes fetched: ', d['SpiderID'].__len__())
             check_pages[page] = 1
             # print('FETCHED: ', page)
+    print(f"Total features: {d.keys()}\nTotal Resumes Extracted: {d['SpiderID'].__len__()}")
     # soup_plus = BeautifulSoup(requests.get(resume_doc).text, 'lxml')
     # content = soup_plus.find(id='Table3').parent
     # for i in soup_plus.find_all('font', attrs={'color': ['#000000', '#000f99']}):
