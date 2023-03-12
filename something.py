46b55a353ede62e53ad726eb82012bc47f1fb0c8
p1utoze <aditya.n.awati@gmail.com>
2023-03-12 21:40:20 +0530

    Update something.py

--- something.py
+++ something.py
@@ -4,12 +4,13 @@ import sys
 parser = argparse.ArgumentParser(
                                  description='A script to scrape Resumes of various domains'
                                              'listed in www.jobspider.com',
-                                 allow_abbrev=True)
+                                 allow_abbrev=True,)
 
 no_args_group = parser.add_mutually_exclusive_group(required=False)
 no_args_group.add_argument('-d', '--description', action='store_true', help='Print program description')
 
-# parser.add_argument('')
+parser.add_argument('-n', '--domain', action='stored_const', type=str)
+
 args = parser.parse_args()
 
 if args.description:
