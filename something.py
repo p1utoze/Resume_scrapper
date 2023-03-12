b04b11ac3f95e54b6a1127f5c4ce9df9dbcb91dd
p1utoze <aditya.n.awati@gmail.com>
2023-03-12 22:42:13 +0530

    Update something.py

--- something.py
+++ something.py
@@ -1,20 +1,24 @@
 from scrape import scrape
 import argparse
 import sys
-parser = argparse.ArgumentParser(
+parser = argparse.ArgumentParser(prog='scraper',
                                  description='A script to scrape Resumes of various domains'
                                              'listed in www.jobspider.com',
                                  allow_abbrev=True,)
 
-no_args_group = parser.add_mutually_exclusive_group(required=False)
-no_args_group.add_argument('-d', '--description', action='store_true', help='Print program description')
+# no_args_group = parser.add_mutually_exclusive_group(required=False)
+# no_args_group.add_argument('-d', '--description', action='store_true', help='Print program description')
 
-parser.add_argument('-n', '--domain', action='stored_const', type=str)
-
+myargs_group = parser.add_mutually_exclusive_group(required=False)
+myargs_group.add_argument('-n', '--domain', type=str, action='store')
+myargs_group.add_argument('-c', '--category', action='store')
 args = parser.parse_args()
 
-if args.description:
+if args.domain:
     print('hi')
+
 else:
     print(parser.description)
+    print('Running default search: engineer.....')
+    d = scrape()
     exit()
