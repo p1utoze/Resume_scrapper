de68ca5a57a76d46529c504b9e113da0d3241765
p1utoze <aditya.n.awati@gmail.com>
2023-03-12 20:46:42 +0530

    Update something.py

--- something.py
+++ something.py
@@ -1,5 +1,9 @@
 from scrape import scrape
 import argparse
 import re
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+                                 description='A script to scrape Resumes of various domains'
+                                             'listed in www.jobspider.com',
+                                 allow_abbrev=True)
 parser.add_argument('')
+parser.parse_args()
